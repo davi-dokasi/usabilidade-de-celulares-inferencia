994138555908703f58cc475aa56f73f79b105c23
Davi Augusto <daviaugusto2509@gmail.com>
2024-02-13 12:16:51 -0300

    Adicionando o quarto presentation, como esqueleto para construção posterior.// Adicionado o Gitgnore também, para livre uso.

--- trabalho_2.r
+++ trabalho_2.r
@@ -17,20 +17,34 @@ homens
 mens <- filter(dt, gender == 'male')
 media_mens <- mean(mens$tempo_no_dia)
 
-x1 <- homens * media_mens
-x1
-
-x2 <- filter(dt$gender %in% c('female','male'))
-
 ## Mulheres
 mulheres <- nrow(filter(dt, gender == 'female'))
 mulheres
 wmns <- filter(dt, gender == 'female')
-media_wmns <- (wmns$tempo_no_dia)
+media_wmns <- mean(wmns$tempo_no_dia)
+
+## Teste proporções
+# Váriáveis do teste
+
+x2 <- dt |> filter(gender %in% c('female','male')) # Filtragem dos sexos
+media_tempo <- mean(x2$tempo_no_dia) # Média do tempo de uso por dia
+tamn <- length(x2$tempo_no_dia) # Tamanho do dataset
+
+medi <- ((media_mens * homens) + (media_wmns * mulheres) )
+medi/ tamn
+dp <- medi/ tamn # Estimativa ponderada -> Pchapéu
+
+# Aplicação da fórmula
+z_value <- (media_mens - media_wmns) / (sqrt((dp * (1 - dp)) * ((1/homens + 1/mulheres ))))
+z_value
+
+alfa <- 0.05
+regiao_critc <- alfa/2
+qnorm(regiao_critc)
 
 
+## Bagunça
 mens$tempo_no_dia
 nrow(mens$tempo_no_dia)
 # database com/ vetor com mean, não precisa do mean, só vetor embaixo
-
 prop.test(c(mens$tempo_no_dia, wmns$tempo_no_dia), c(homens, mulheres), conf.level = 0.95)
